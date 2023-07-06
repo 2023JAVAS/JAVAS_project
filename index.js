857b3ab14d10d654db52e57d146ceccf0bc0df0e
unknown <sumini94@naver.com>
2023-07-06 17:49:22 +0900

    index 수민 업로드

---
+++ index.js
@@ -0,0 +1,9 @@
+// 햄버거메뉴 열고 닫기
+$(function () {
+  $('.checkbox').on('click', () => {
+    $('.header').toggleClass('active');
+    $('.logo').toggleClass('active');
+    $('.checkmark span').toggleClass('active');
+    $('.menu').slideToggle(); // == display: block
+  });
+});
