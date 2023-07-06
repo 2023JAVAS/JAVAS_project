4c6de401b3ea4063249a212b2be003726f8e8fb7
2yeji2 <ii316@naver.com>
2023-07-06 22:25:42 +0900

    허니문빌라, 베이스위트 객실 페이지 완료 및 푸터부분 수정, readme 파일 작성 -예지

---
+++ room/room_bay_suite/room_bay_suite.js
@@ -0,0 +1,41 @@
+// a가 가지고 있는 기본 이벤트 제거
+$('a[href="#"]').on("click", (evt) => {
+  evt.preventDefault();
+});
+
+//  첫번째 슬라이드 : swiper 플러그인 사용
+const mySwiper1 = new Swiper(".mySwiper1", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//  두번째 슬라이드 : swiper 플러그인 사용
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// 객실 도면 팝업창
+function popup() {
+  var url =
+    "https://2023javas.github.io/JAVAS_project/room/room_bay_suite/images/room_plan.jpg";
+  var name = "popup";
+  var option =
+    "width = 700, height = 800, top = 100, left = 600, location = no";
+  window.open(url, name, option);
+}
