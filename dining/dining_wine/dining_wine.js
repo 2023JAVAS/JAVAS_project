99fd654e03f03b332ef1870a9c3ec54394ba2da4
jieun822 <solvit0822@naver.com>
2023-07-06 17:08:14 +0900

    230706 지은- 다이닝, 부대시설 서브페이지 완료

---
+++ dining/dining_wine/dining_wine.js
@@ -0,0 +1,31 @@
+// a가 가지고 있는 기본 이벤트 제거
+$('a[href="#"]').on("click", (evt) => {
+  evt.preventDefault();
+});
+
+//  첫번째 슬라이드 : swiper 플러그인 사용
+const mySwiper1 = new Swiper(".mySwiper1", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//  두번째 슬라이드 : swiper 플러그인 사용
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+});
