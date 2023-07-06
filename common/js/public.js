b0edb21d53d81fa85961a775a904fefd88ced1dc
2yeji2 <ii316@naver.com>
2023-07-06 16:53:20 +0900

    파일추가

--- common/js/public.js
+++ common/js/public.js
@@ -1,9 +1,7 @@
 // 햄버거메뉴 열고 닫기
-$(function () {
-  $(".checkbox").on("click", () => {
-    $(".header").toggleClass("active");
-    $(".logo").toggleClass("active");
-    $(".checkmark span").toggleClass("active");
-    $(".menu").slideToggle(); // == display: block
-  });
+$(".checkbox").on("click", () => {
+  $(".header").toggleClass("active");
+  $(".logo").toggleClass("active");
+  $(".checkmark span").toggleClass("active");
+  $(".menu").slideToggle(); // == display: block
 });
