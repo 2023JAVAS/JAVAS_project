8bbfefca2dccaa816b0754b24e7d578f90e394af
geon3017 <geon3017@naver.com>
2023-07-06 20:46:26 +0900

    README 230706 최건 좌우롤링 추가완료

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ $(function () {
     $('.menu').slideToggle(); // == display: block
   });
 });
+
+// 좌우 스크롤 롤링 230706 좌우롤링 위해 작성 최건
+window.onscroll = function () {
+  scrollRotate(); // DOM에서 함수 onscroll(),를 호출할 함수 를 만듭니다 scrollRotate()
+  };
+
+  function scrollRotate() { 
+  // id를 통해 reload 요소를 찾으면 스크롤하여 회전하고 image변수에 저장합니다 .
+  let smile = document.getElementById("top");
+  // 문서가 현재 세로축을 따라 스크롤되는 픽셀수를 반환하는 Window.pageYOffset 속성을 적용 합니다.
+  smile.style.transform = "translateX(" + window.pageYOffset/-15 + "px)";
+
+  let rombo = document.getElementById("bottom");
+  // 문서가 현재 세로축을 따라 스크롤되는 픽셀수를 반환하는 Window.pageYOffset 속성을 적용 합니다.
+  rombo.style.transform = "translateX(" + window.pageYOffset/15 + "px)";
+
+  }
