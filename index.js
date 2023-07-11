1e1da7d09138bd6a9251befec462a4cbd480b524
geon3017 <geon3017@naver.com>
2023-07-11 17:40:41 +0900

    REMAD 230711 예약날짜설정 완료 최건

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // 햄버거메뉴 열고 닫기
 $(function () {
-  $(".checkbox").on("click", () => {
-    $(".header").toggleClass("active");
-    $(".logo").toggleClass("active");
-    $(".checkmark span").toggleClass("active");
-    $(".menu").slideToggle(); // == display: block
+  $('.checkbox').on('click', () => {
+    $('.header').toggleClass('active');
+    $('.logo').toggleClass('active');
+    $('.checkmark span').toggleClass('active');
+    $('.menu').slideToggle(); // == display: block
   });
 });
 
@@ -15,22 +15,22 @@ window.onscroll = function () {
 
 function scrollRotate() {
   // id를 통해 reload 요소를 찾으면 스크롤하여 회전하고 image변수에 저장합니다 .
-  let smile = document.getElementById("top");
+  let smile = document.getElementById('top');
   // 문서가 현재 세로축을 따라 스크롤되는 픽셀수를 반환하는 Window.pageYOffset 속성을 적용 합니다.
-  smile.style.transform = "translateX(" + window.pageYOffset / -4 + "px)";
+  smile.style.transform = 'translateX(' + window.pageYOffset / -4 + 'px)';
 
-  let rombo = document.getElementById("bottom");
+  let rombo = document.getElementById('bottom');
   // 문서가 현재 세로축을 따라 스크롤되는 픽셀수를 반환하는 Window.pageYOffset 속성을 적용 합니다.
-  rombo.style.transform = "translateX(" + window.pageYOffset / 3 + "px)";
+  rombo.style.transform = 'translateX(' + window.pageYOffset / 3 + 'px)';
 }
 
 //스크롤시 이미지 크게 보이기
-gsap.set(".scaleDown", { xPercent: -50, yPercent: -50 });
-gsap.to(".scaleDown", {
+gsap.set('.scaleDown', { xPercent: -50, yPercent: -50 });
+gsap.to('.scaleDown', {
   scale: 2,
   scrollTrigger: {
-    trigger: ".main_animation",
-    pin: ".main_animation",
+    trigger: '.main_animation',
+    pin: '.main_animation',
     scrub: true,
   },
 });
--- reservation/reservation.js
+++ reservation/reservation.js
@@ -31,42 +31,112 @@ const handleSelect = (label, item) => {
 };
 
 // 클릭시 달력
-// https://www.daterangepicker.com/
-$(function () {
-  $('input[name="daterange"]').daterangepicker(
-    {
-      opens: 'left',
-    },
-    function (start, end, label) {
-      console.log(
-        'A new date selection was made: ' +
-          start.format('YYYY-MM-DD') +
-          ' to ' +
-          end.format('YYYY-MM-DD')
-      );
+var CDate = new Date();
+var today = new Date();
+var selectCk = 0;
+
+var buildcalendar = function () {
+  var htmlDates = '';
+  var prevLast = new Date(CDate.getFullYear(), CDate.getMonth(), 0); //지난 달의 마지막 날
+  var thisFirst = new Date(CDate.getFullYear(), CDate.getMonth(), 1); //이번 달의 첫쨰 날
+  var thisLast = new Date(CDate.getFullYear(), CDate.getMonth() + 1, 0); //이번 달의 마지막 날
+  document.querySelector('.year').innerHTML = CDate.getFullYear() + '년'; // year에 년도 출력
+  document.querySelector('.month').innerHTML = CDate.getMonth() + 1 + '월'; //month에 월 출력
+  const dates = [];
+  if (thisFirst.getDay() != 0) {
+    for (var i = 0; i < thisFirst.getDay(); i++) {
+      dates.unshift(prevLast.getDate() - i); // 지난 달 날짜 채우기
     }
-  );
-});
-
-// 버튼 증가
-
-function count(type) {
-  // 결과를 표시할 element
-  const resultElement = document.getElementById('result');
-
-  // 현재 화면에 표시된 값
-  let number = resultElement.innerText;
-
-  // 더하기/빼기
-  if (type === 'plus') {
-    number = parseInt(number) + 1;
-  } else if (type === 'minus') {
-    number = parseInt(number) - 1;
+  }
+  for (var i = 1; i <= thisLast.getDate(); i++) {
+    dates.push(i); // 이번 달 날짜 채우기
+  }
+  for (var i = 1; i <= 13 - thisLast.getDay(); i++) {
+    dates.push(i); // 다음 달 날짜 채우기 (나머지 다 채운 다음 출력할 때 42개만 출력함)
   }
 
-  // 결과 출력
-  resultElement.innerText = number;
+  for (var i = 0; i < 42; i++) {
+    if (i < thisFirst.getDay()) {
+      htmlDates += '<div class="date last">' + dates[i] + '</div>';
+    } else if (
+      today.getDate() == dates[i] &&
+      today.getMonth() == CDate.getMonth() &&
+      today.getFullYear() == CDate.getFullYear()
+    ) {
+      htmlDates +=
+        '<div id="date_' +
+        dates[i] +
+        '" class="date today" onclick="fn_selectDate(' +
+        dates[i] +
+        ');">' +
+        dates[i] +
+        '</div>';
+    } else if (i >= thisFirst.getDay() + thisLast.getDate()) {
+      htmlDates += '<div class="date next">' + dates[i] + '</div>';
+    } else {
+      htmlDates +=
+        '<div id="date_' +
+        dates[i] +
+        '" class="date" onclick="fn_selectDate(' +
+        dates[i] +
+        ');">' +
+        dates[i] +
+        '</div>';
+    }
+  }
+  document.querySelector('.dates').innerHTML = htmlDates;
+};
+
+function prevCal() {
+  CDate.setMonth(CDate.getMonth() - 1);
+  buildcalendar();
 }
+function nextCal() {
+  CDate.setMonth(CDate.getMonth() + 1);
+  buildcalendar();
+}
+
+function fn_selectDate(date) {
+  var year = CDate.getFullYear();
+  var month = CDate.getMonth() + 1;
+  var date_txt = '';
+  if (CDate.getMonth + 1 < 10) {
+    month = '0' + (CDate.getMonth() + 1);
+  }
+  if (date < 10) {
+    date_txt = '0' + date;
+  }
+
+  if (selectCk == 0) {
+    $('.date').css('background-color', '');
+    $('.date').css('color', '');
+    $('#date_' + date).css('background-color', 'red');
+    $('#date_' + date).css('color', 'white');
+
+    $('#period_1').val(year + '-' + month + '-' + date);
+    $('#period_2').val('');
+    selectCk = date;
+  } else {
+    $('#date_' + date).css('background-color', 'red');
+    $('#date_' + date).css('color', 'white');
+    for (var i = selectCk + 1; i < date; i++) {
+      $('#date_' + i).css('background-color', '#FFDDDD');
+    }
+
+    $('#period_2').val(year + '-' + month + '-' + date);
+    selectCk = 0;
+  }
+}
+
+buildcalendar();
+
+// 달력 들어갔다 나왔다 버튼
+$(document).ready(function () {
+  $('#button').click(function () {
+    $('.calendar').toggle();
+  });
+});
+// 버튼 증가
 
 new Vue({
   el: '#app',
