3fb9fd473a06e78bef1b6251a0d599e64a264757
geon3017 <geon3017@naver.com>
2023-07-11 09:17:41 +0900

    REAMD 최건 예약 증가버튼 완료 230711

--- reservation/reservation.js
+++ reservation/reservation.js
@@ -47,3 +47,68 @@ $(function () {
     }
   );
 });
+
+// 버튼 증가
+
+function count(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result');
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === 'plus') {
+    number = parseInt(number) + 1;
+  } else if (type === 'minus') {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+new Vue({
+  el: '#app',
+  data: {
+    count: 0,
+  },
+  methods: {
+    countUp: function () {
+      this.count++;
+    },
+    countDown: function () {
+      this.count--;
+    },
+  },
+});
+
+new Vue({
+  el: '#adult',
+  data: {
+    count: 0,
+  },
+  methods: {
+    countUp: function () {
+      this.count++;
+    },
+    countDown: function () {
+      this.count--;
+    },
+  },
+});
+
+new Vue({
+  el: '#children',
+  data: {
+    count: 0,
+  },
+  methods: {
+    countUp: function () {
+      this.count++;
+    },
+    countDown: function () {
+      this.count--;
+    },
+  },
+});
